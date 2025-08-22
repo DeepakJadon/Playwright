48c7b5e29326e37705231993dbf37927977e9163
Deepak Jadon <todeepak086@gmail.com>
2025-08-22 20:41:23 +0530

    only for n8n branch

--- tests/fileuploaddownload.spec.js
+++ tests/fileuploaddownload.spec.js
@@ -1,25 +1,25 @@
-const { test } = require('@playwright/test');
-const path = require('path');
+// const { test } = require('@playwright/test');
+// const path = require('path');
 
-test('File upload example', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/upload');
-  const filePath = path.join(__dirname, 'sample.txt');
-  await page.setInputFiles('#file-upload', filePath);
-  await page.waitForTimeout(3000)
-  await page.click('#file-submit');
-  await page.waitForSelector('h3:has-text("File Uploaded!")');
-  await page.waitForTimeout(3000)
-});
+// test('File upload example', async ({ page }) => {
+//   await page.goto('https://the-internet.herokuapp.com/upload');
+//   const filePath = path.join(__dirname, 'sample.txt');
+//   await page.setInputFiles('#file-upload', filePath);
+//   await page.waitForTimeout(3000)
+//   await page.click('#file-submit');
+//   await page.waitForSelector('h3:has-text("File Uploaded!")');
+//   await page.waitForTimeout(3000)
+// });
 
 
-test('File download example', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/download');
-  const downloadPromise = page.waitForEvent('download');
-  await page.click('text=some-file.txt'); 
+// test('File download example', async ({ page }) => {
+//   await page.goto('https://the-internet.herokuapp.com/download');
+//   const downloadPromise = page.waitForEvent('download');
+//   await page.click('text=some-file.txt'); 
 
-  // Wait for the download to complete
-  const download = await downloadPromise;
-  const downloadPath = path.join(__dirname, await download.suggestedFilename());
-  await download.saveAs(downloadPath);
-  console.log(`File downloaded to: ${downloadPath}`);
-});
+//   // Wait for the download to complete
+//   const download = await downloadPromise;
+//   const downloadPath = path.join(__dirname, await download.suggestedFilename());
+//   await download.saveAs(downloadPath);
+//   console.log(`File downloaded to: ${downloadPath}`);
+// });
--- tests/testfixture.spec.js
+++ tests/testfixture.spec.js
@@ -1,9 +1,9 @@
 
 
-const { test } = require('../pages/customfixture');
-const { expect } = require('@playwright/test');
+// const { test } = require('../pages/customfixture');
+// const { expect } = require('@playwright/test');
 
-test('print custom fixture value', async ({ myName }) => {
-  console.log('Fixture says:', myName);  // QA Automation Engineer 😄
-  expect(myName).toContain('Deepak');
-});
+// test('print custom fixture value', async ({ myName }) => {
+//   console.log('Fixture says:', myName);  // QA Automation Engineer 😄
+//   expect(myName).toContain('Deepak');
+// });
